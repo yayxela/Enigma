ef4d19272161d7c481a619055b9e06f46390e8b5
yayxela <37211398+yayxela@users.noreply.github.com>
2019-10-01 19:21:28 +0600

    Add files via upload

---
+++ ComplicatedEnigma/ComplicatedEnigma/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ComplicatedEnigma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Взять текст";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 41);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(309, 98);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Зашифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(117, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Расшифровать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(327, 41);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(358, 145);
+            this.textBox6.TabIndex = 13;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(222, 163);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Сохранить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 211);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ComplicatedEnigma/ComplicatedEnigma/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComplicatedEnigma
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        static byte[][] rotor = new byte[4][];
+        static byte[][] switching = new byte[4][]; 
+        static byte[] plainText, cText, shift, alphabet = new byte[256];
+        static byte[] key;
+        static byte p1, p2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 4; i++)
+            {
+                rotor[i] = new byte[256];
+                switching[i] = new byte[16];
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                alphabet[i] = (byte)i;
+            }
+
+            shift = new byte[4];
+            key = new byte[16];
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            plainText = null;
+            textBox5.Text = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = ofd.FileName;
+                plainText = File.ReadAllBytes(fileName);
+                Console.WriteLine();
+                foreach (var item in plainText)
+                {
+                    textBox5.Text += (byte)item + ", ";  
+                }
+                Console.WriteLine(plainText.Length);
+                ofd.Dispose();
+            }
+            key = File.ReadAllBytes("key.txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+             cText = null;
+            textBox6.Text = "";
+            byte[] result = Crypt(plainText, true);
+            Console.WriteLine(result.Length);
+            foreach (var item in Crypt(plainText, true))
+                textBox6.Text += item + ", ";
+        }
+
+        public void button4_Click(object sender, EventArgs e)
+        {
+            cText = null;
+            textBox6.Text = "";
+            byte[] result = Crypt(plainText, false);
+            Console.WriteLine(result.Length);
+            foreach (var item in Crypt(plainText, false))
+                textBox6.Text += item + ", ";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine();
+            foreach (var item in cText)
+            {
+                Console.Write(item + " ");
+            }
+            File.WriteAllBytes("cyphered text.txt", cText);
+        }
+
+        public void PRVG()
+        {
+            byte[] bytes = new byte[276 + 16]; // 256rotors 16switch 4shift 
+            byte[] sequence = new byte[276];
+
+            //заполняем начальный массив байтов ключом
+            for (int i = 0; i < key.Length; i++)
+            {
+                bytes[i] = key[i];
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                rotor[i][j] = (byte)j;
+                }
+            }
+            
+            for (int i = 16; i < bytes.Length; i++)
+            {
+                bytes[i] = (byte)((bytes[i - 16] + bytes[i - 5]) % 256);
+            }
+            sequence = bytes.Skip(16).ToArray();
+            for (int i = 0; i < 4; i++)
+            {
+                switching[i] = sequence.Skip(i*16).Take(16).ToArray();
+            }
+            //switching = sequence.Take(16).ToArray();
+
+            byte S = 0;
+            byte helper = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    S = rotor[i][(S + rotor[i][j % 256] + switching[i][j % 16]) % 256];
+                    helper = rotor[i][j % 256];
+                    rotor[i][j % 256] = rotor[i][S];
+                    rotor[i][S] = helper;
+                    
+                }    
+            }
+
+            
+
+            //заполняем сдвиги
+            shift = sequence.Skip(64).Take(4).ToArray();
+            foreach (var s in shift)
+            {
+                Console.Write(s + "- s ");
+            }
+            Console.WriteLine(Environment.NewLine);
+            //return sequence;
+        }
+
+        public byte[] Crypt(byte[] text, bool method)
+        {
+            int[] shifting = new int[4];
+            cText =new byte[text.Length];
+            PRVG();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (method == true)
+                {
+
+                    cText[i] = RotorForaward(RotorForaward(RotorForaward(RotorForaward(text[i], rotor[0], shifting[0]), rotor[1], shifting[1]), rotor[2], shifting[2]), rotor[3], shifting[3]);
+                    Console.Write($"{cText[i]} shift0 = {shifting[0]}, shift1 = {shifting[1]}, shift2 = {shifting[2]}, shift3 = {shifting[3]} ");
+                    Console.WriteLine();
+                }
+                if (method == false)
+                {  
+                    cText[i] = RotorBackward(RotorBackward(RotorBackward(RotorBackward(text[i], rotor[3], shifting[3]), rotor[2], shifting[2]), rotor[1], shifting[1]), rotor[0], shifting[0]);
+                    Console.Write($"{cText[i]} shift3 = {shifting[3]}, shift2 = {shifting[2]}, shift1 = {shifting[1]}, shift0 = {shifting[0]} ");
+                }
+                Console.WriteLine(shift.Length);
+                for (int j = 0; j < shift.Length; j++) {
+                    shifting[j] += shift[j];
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine();
+            return (cText);
+        }
+
+        public static byte[] Switching(byte[] rotor)
+        {
+            byte S = 0, helper;
+            for (int i = 0; i < rotor.Length; i++)
+            {
+                S = rotor[(S + rotor[i % 256] + switching[i][i % 16]) % 256];
+                helper = rotor[i % 256];
+                rotor[i % 256] = rotor[S];
+                rotor[S] = helper;
+            }
+            return rotor;
+        }
+
+        public static byte RotorForaward(int text, byte[] rotAlph, int shift)
+        {
+
+            Console.WriteLine("new index " + ((text + shift) % 256));
+            return rotAlph[(text + shift) % 256];
+        }
+        public static byte RotorBackward(int text, byte[] rotAlph, int shift)
+        {
+            return alphabet[(Array.IndexOf(rotAlph, text) + 256 * ((Array.IndexOf(rotAlph, text) < shift % 256) ? 1 : 0) - shift %256)];
+        }
+
+        static private int Mod(int a,int b)
+        {
+            return (b + a % b) % b;
+        }
+    }
+}
+//[1][2][3*][4][5][6_][7][8]
+
+//генерируем последовательность
+//заполняем роторы
+//заполняем массив перестановок
+//определение сдвигов 
+//i = 115 rotAlph = 9, i = 9 rotAlph = 73, i = 73 rotAlph = 221, i = 221 rotAlph = 163, 
+
+
+//new index 105
+//new index 64
+//new index 199
+//new index 202
+//160 shift0 = 0, shift1 = 0, shift2 = 0, shift3 = 0 
+//4
+
+//new index 143
+//new index 44
+//new index 13
+//new index 88
+//99 shift0 = 111, shift1 = 246, shift2 = 19, shift3 = 191 
